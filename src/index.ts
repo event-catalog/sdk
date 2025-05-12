78172bf3d05145d3042db751777b6968d57f102b
David Boyne <boyneyy123@gmail.com>
2025-05-12 09:33:45 +0100

    feat(sdk): added util function to get a path to a resource

--- src/index.ts
+++ src/index.ts
@@ -75,6 +75,10 @@ import {
   addMessageToChannel,
 } from './channels';
 
+import { 
+  getResourcePath
+} from './internal/resources';
+
 import { dumpCatalog } from './eventcatalog';
 
 import { writeCustomDoc, getCustomDoc, getCustomDocs, rmCustomDoc } from './custom-docs';
@@ -785,5 +789,19 @@ export default (path: string) => {
      * @returns A JSON file with the catalog
      */
     dumpCatalog: dumpCatalog(join(path)),
+
+
+    /**
+     * ================================
+     *            Resources Utils
+     * ================================
+     */
+
+    /**
+     * Returns the path to a given resource by id and version
+     */
+    getResourcePath: getResourcePath,
+
+
   };
 };
